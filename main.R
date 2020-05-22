2aa9b0d7bbe3642fd04c278eeb39b88ee3ae5086
Mikhail <16112000m@gmail.com>
2020-05-22 19:51:17 +0300

    - new tasks

--- main.R
+++ main.R
@@ -9,3 +9,27 @@ my_names <- function (dataset, names){
 }
 
 my_names(test_data, names)
+
+# В этой задаче мы хотели бы вытащить из каждого списка только значение p - value.
+# То есть получить новый список или вектор только с одним показателем (p - value) для каждой переменной.
+
+normality_tests_p <- lapply(iris[, 1:4], shapiro.test)
+s2 <- sapply(normality_tests, '[')
+s2[2,]
+
+# Напишите функцию one_sample_t, которая получает на вход два аргумента:
+# 1. Dataframe произвольного размера с произвольным числом переменных различного типа.
+# 2. Числовое значение среднего в генеральной совокупности.
+# Ваша функция должна применять одновыборочный t - test к каждой числовой переменной в данных, и сравнивать среднее
+# значение этой переменной с указанным значением среднего в генеральной совокупности (второй аргумент функции).
+# Функция должна возвращать список, где каждый элемент это вектор, состоящий из t - значения, числа степеней свобод
+# (df) и значения p - value.
+
+one_sample_t <- function(test_data, general_mean){
+  tested <- test_data[sapply(test_data, is.numeric)]
+  res <- lapply(tested, function(x) c(t.test(x,mu=general_mean)$statistic,t.test(x,mu=general_mean)$parameter, t.test(x,mu=general_mean)$p.value))
+  return(res)
+}
+
+one_sample_t(iris[, 1:4], 4)
+
